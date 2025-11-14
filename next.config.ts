589e89d1b35c689b76eda563c67c50d6e33ea458
Carmelo Canavan <74259203+Ninja-5000@users.noreply.github.com>
2025-11-14 18:12:03 +0400

    [chore] Update project name and next.config for v2

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "api.statusbadges.me",
+      },
+    ],
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
