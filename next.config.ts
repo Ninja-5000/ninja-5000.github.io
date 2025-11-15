0b6aaa6ea24c470c57e946348294e22ef5bbac29
Carmelo Canavan <74259203+Ninja-5000@users.noreply.github.com>
2025-11-16 00:00:21 +0400

    [fix] gh page aaa

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  assetPrefix: "https://ninja-5000.github.io/",
   images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "api.statusbadges.me",
-      },
-    ],
     unoptimized: true,
   },
 };
