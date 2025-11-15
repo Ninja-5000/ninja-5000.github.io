7115e96449e03a9a1df1bea828e4e3a8432320b5
Carmelo Canavan <74259203+Ninja-5000@users.noreply.github.com>
2025-11-15 12:22:09 +0400

    [fix] Github pages config

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  basePath:
-    process.env.NODE_ENV === "production" ? "/ninja-5000.github.io" : "",
-  trailingSlash: true,
   images: {
     remotePatterns: [
       {
